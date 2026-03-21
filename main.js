2803a2162b5d3225e17c44e837c8684168d68975
Ashish Singodiya <ashishsingodiya1@gmail.com>
2026-03-21 17:51:02 +0530

    Implement monaco editor

--- main.js
+++ main.js
@@ -21,14 +21,15 @@ function applyChaiClassesToDoc(doc) {
 applyChaiClassesToDoc(document);
 
 function setupPlayground() {
-  const input = document.getElementById("chai-playground-input");
   const preview = document.getElementById("chai-playground-preview");
-  // console.log(preview.contentDocument.body);
+  const defaultHtml = `<div class="chai-p-16 chai-border-2 chai-border-orange chai-rounded-md chai-m-10 chai-flex chai-flex-col chai-items-center">
+  <h3 class="chai-text-orange chai-m-0">Hello from Playground</h3>
+  <p class="chai-text-gray chai-mt-10">Edit this HTML on the left side.</p>
+  <button class="chai-bg-orange chai-text-white chai-px-12 chai-py-8 chai-rounded-full chai-mt-10">Try Now</button>
+</div>`;
 
-  const renderPreview = () => {
+  const renderPreview = (code) => {
     const previewDoc = preview.contentDocument || preview.contentWindow?.document;
-
-    const code = input.value;
     const html = `<!DOCTYPE html> <html> <head></head><body>${code}</body> </html>`;
     previewDoc.open();
     previewDoc.write(html);
@@ -36,8 +37,24 @@ function setupPlayground() {
     applyChaiClassesToDoc(previewDoc);
   };
 
-  input.addEventListener("input", renderPreview);
-  renderPreview();
+  require.config({ paths: { vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.45.0/min/vs" } });
+
+  require(["vs/editor/editor.main"], function () {
+    const editor = monaco.editor.create(document.getElementById("editor-container"), {
+      value: defaultHtml,
+      language: "html",
+      theme: "vs-dark",
+      wordWrap: "on",
+      minimap: { enabled: false },
+      automaticLayout: true,
+    });
+
+    renderPreview(editor.getValue());
+
+    editor.onDidChangeModelContent(() => {
+      renderPreview(editor.getValue());
+    });
+  });
 }
 
 setupPlayground();
